e8622536ca51ae01e43ad30743dfcb615b4a3813
Administrator <Administrator@192.168.40.1>
2019-08-16 16:25:52 +0800

    更新了注册页面和surprise

--- NetworkClub/src/domain/stu.java
+++ NetworkClub/src/domain/stu.java
@@ -9,5 +9,58 @@ public class stu {
 	String gender;
 	String tele;
 	String qq;
+	@Override
+	public String toString() {
+		return "stu [school=" + school + ", college=" + college + ", specialty=" + specialty + ", name=" + name
+				+ ", num=" + num + ", gender=" + gender + ", tele=" + tele + ", qq=" + qq + "]";
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public String getSpecialty() {
+		return specialty;
+	}
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getTele() {
+		return tele;
+	}
+	public void setTele(String tele) {
+		this.tele = tele;
+	}
+	public String getQq() {
+		return qq;
+	}
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
 
 }
--- NetworkClub/src/servlet/login.java
+++ NetworkClub/src/servlet/login.java
@@ -1,7 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -11,7 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
+import domain.stu;
 import util.JdbcUtil;
 
 /**
@@ -41,32 +46,40 @@ public class login extends HttpServlet {
 		String gender = request.getParameter("gender");
 		String tele = request.getParameter("tele");
 		String qq = request.getParameter("qq");
-		System.out.println(school);
-		System.out.println(gender);
-		if (school.equals("m"))
+//		System.out.println(school);
+//		System.out.println(college);
+//		System.out.println(specialty);
+//		System.out.println(name);
+//		System.out.println(num);
+//		System.out.println(tele);
+//		System.out.println(qq);
+////		System.out.println(gender);
+//		System.out.println(gender);
+
+		if (school.equals("z"))
 			school = "张家港校区";
 		else
 			school = "苏州理工学院";
-		if (gender.equals("m"))
+		if (gender.equals("s"))
 			gender = "男";
 		else
 			gender = "女";
-		System.out.println(school);
-		System.out.println(college);
-		System.out.println(specialty);
-		System.out.println(name);
-		System.out.println(num);
-		System.out.println(gender);
-		System.out.println(tele);
-		System.out.println(qq);
 		String sql = "insert into stu(school,college,specialty,name,num,gender,tele,qq) value(?,?,?,?,?,?,?,?)";
 		try {
 			QueryRunner qr = new QueryRunner(JdbcUtil.getDs());
 			qr.update(sql, school, college, specialty, name, num, gender, tele, qq);
+			String sql1="select *from stu";
+			List<stu> list = qr.query(sql1, new BeanListHandler<stu>(stu.class));
+			for (stu stu0 : list) {
+				System.out.println(stu0);
+			}
 			response.setCharacterEncoding("UTF-8");
 			response.setHeader("Content-Type", "text/html;charset=UTF-8");
-//			response.getWriter().write("提交成功");
+////			response.getWriter().write("提交成功");
 			response.sendRedirect("http://localhost:8080/NetworkClub/index.html");
+			for (stu stu : list) {
+				response.getWriter().write(stu.toString());
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
